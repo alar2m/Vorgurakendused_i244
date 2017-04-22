331d10517e2a8b58e2812bc918c5b870e5d44d82
alar2m <alar2magi@gmail.com>
2017-04-22 18:50:07 +0300

    Update praktikum.php

--- Praktikum/praktikum.php
+++ Praktikum/praktikum.php
@@ -3,8 +3,6 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-
-
 ?>
 <!DOCTYPE html>
 <html>
@@ -33,8 +31,6 @@ echo  'Current PHP version: ' . phpversion();
 ?>
 </div>
 <br>
-
-
 <p>
  <a href="http://validator.w3.org/check?uri=referer">
   <img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" height="31" width="88" />
